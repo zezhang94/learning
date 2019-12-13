a11afcf6b7dfd0ffb3d8f56a859402606e574edf
yorkzhang <yorkzhang1994@outlook.com>
2019-12-13 13:55:57 +0800

    Remove useless import.

--- leetcode-solution/src/main/java/easy/MinimumIndexSumOfTwoLists.java
+++ leetcode-solution/src/main/java/easy/MinimumIndexSumOfTwoLists.java
@@ -1,13 +1,8 @@
 package easy;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * # 599
