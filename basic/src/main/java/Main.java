8f439d11651738aa8b96aac6436e0a66a9592451
yorkzhang <yorkzhang1994@outlook.com>
2020-01-06 17:07:52 +0800

    Initialize.

--- basic/src/main/java/Main.java
+++ basic/src/main/java/Main.java
@@ -1,6 +1,17 @@
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Bag<String> testBag = new Bag<>();
+        testBag.add("element");
+
+        Queue<Integer> testQueue = new Queue<>();
+        testQueue.enqueue(1);
+
+        Stack<Integer> testStack = new Stack<>();
+        testStack.push(1);
     }
 }
---
+++ basic/src/main/java/fundamentals/BinarySearch.java
@@ -0,0 +1,107 @@
+package fundamentals;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+/******************************************************************************
+ *  Compilation:  javac BinarySearch.java
+ *  Execution:    java BinarySearch whitelist.txt < input.txt
+ *  Dependencies: In.java StdIn.java StdOut.java
+ *  Data files:   https://algs4.cs.princeton.edu/11model/tinyW.txt
+ *                https://algs4.cs.princeton.edu/11model/tinyT.txt
+ *                https://algs4.cs.princeton.edu/11model/largeW.txt
+ *                https://algs4.cs.princeton.edu/11model/largeT.txt
+ *
+ *  % java BinarySearch tinyW.txt < tinyT.txt
+ *  50
+ *  99
+ *  13
+ *
+ *  % java BinarySearch largeW.txt < largeT.txt | more
+ *  499569
+ *  984875
+ *  295754
+ *  207807
+ *  140925
+ *  161828
+ *  [367,966 total values]
+ *
+ ******************************************************************************/
+public class BinarySearch {
+
+    /**
+     * This class should not be instantiated.
+     */
+    private BinarySearch() { }
+
+    /**
+     * Returns the index of the specified key in the specified array.
+     *
+     * @param  a the array of integers, must be sorted in ascending order
+     * @param  key the search key
+     * @return index of key in array {@code a} if present; {@code -1} otherwise
+     */
+    public static int indexOf(int[] a, int key) {
+        if (null == a || 0 == a.length) {
+            return -1;
+        }
+
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (a[mid] == key) {
+                return mid;
+            } else if (key < a[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Returns the index of the specified key in the specified array.
+     * This function is poorly named because it does not give the <em>rank</em>
+     * if the array has duplicate keys or if the key is not in the array.
+     *
+     * @param  key the search key
+     * @param  a the array of integers, must be sorted in ascending order
+     * @return index of key in array {@code a} if present; {@code -1} otherwise
+     * @deprecated Replaced by {@link #indexOf(int[], int)}.
+     */
+    @Deprecated
+    public static int rank(int key, int[] a) {
+        return indexOf(a, key);
+    }
+
+    /**
+     * Reads in a sequence of integers from the whitelist file, specified as
+     * a command-line argument; reads in integers from standard input;
+     * prints to standard output those integers that do <em>not</em> appear in the file.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        args = new String[] {"data/tinyW.txt", "<", "data/tinyT.txt"};
+        //args = new String[] {"data/largeW.txt", "<", "data/largeT.txt"};
+
+        // read the integers from a file
+        In in = new In(args[0]);
+        int[] whitelist = in.readAllInts();
+
+        // sort the array
+        Arrays.sort(whitelist);
+
+        // read integer key from standard input; print if not in whitelist
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (BinarySearch.indexOf(whitelist, key) == -1)
+                StdOut.println(key);
+        }
+    }
+}
