9e594b7c65d581b81869b9cbde2c25ac62c54176
yorkzhang <yorkzhang1994@outlook.com>
2020-04-29 17:31:28 +0800

    Solve new problem.

--- EPI/arrays/delete_duplicates.py
+++ EPI/arrays/delete_duplicates.py
@@ -15,28 +15,54 @@ def delete_duplicates(A):
     i += 1 
   return (ci + 1, A)
 
+# Implement a function which takes as input an array and a key, 
+# and updates the array so that all occurrences of the input key have been removed 
+# and the remaining elements have been shifted left to fill the emptied indices. 
+# Return the number of remaining elements. 
+# There are no requirements as to the values stored beyond the last valid element.
+def variant1(k, A):
+  i, c = 0, 0
+  for i in range(0, len(A)):
+    if A[c] != k:
+      c += 1
+    elif A[c] == k and A[i] != k:
+      A[c] = A[i]
+      A[i] = k
+      c += 1
+  print(A)
+  return c
+
 class Test(unittest.TestCase):
     def test(self):
-        A = [1, 1, 2, 2]
-        B = delete_duplicates(A)
-        self.assertEqual(B[0], 2)
-        self.assertListEqual(B[1], [1, 2, 0, 0])
-        A = [2, 3, 5, 5, 7, 11, 11, 11, 13]
-        B = delete_duplicates(A)
-        self.assertEqual(B[0], 6)
-        self.assertListEqual(B[1], [2, 3, 5, 7, 11, 13, 0, 0, 0])
-        A = [1, 1, 2, 2]
-        B = delete_duplicates(A)
-        self.assertEqual(B[0], 2)
-        self.assertListEqual(B[1], [1, 2, 0, 0])
-        A = [2, 3, 5, 7, 11, 13]
-        B = delete_duplicates(A)
-        self.assertEqual(B[0], 6)
-        self.assertListEqual(B[1], [2, 3, 5, 7, 11, 13])
-        A = [1, 1, 1, 1, 1, 1]
-        B = delete_duplicates(A)
-        self.assertEqual(B[0], 1)
-        self.assertListEqual(B[1], [1, 0, 0, 0, 0, 0])
+      A = [1, 1, 2, 2]
+      B = delete_duplicates(A)
+      self.assertEqual(B[0], 2)
+      self.assertListEqual(B[1], [1, 2, 0, 0])
+      A = [2, 3, 5, 5, 7, 11, 11, 11, 13]
+      B = delete_duplicates(A)
+      self.assertEqual(B[0], 6)
+      self.assertListEqual(B[1], [2, 3, 5, 7, 11, 13, 0, 0, 0])
+      A = [1, 1, 2, 2]
+      B = delete_duplicates(A)
+      self.assertEqual(B[0], 2)
+      self.assertListEqual(B[1], [1, 2, 0, 0])
+      A = [2, 3, 5, 7, 11, 13]
+      B = delete_duplicates(A)
+      self.assertEqual(B[0], 6)
+      self.assertListEqual(B[1], [2, 3, 5, 7, 11, 13])
+      A = [1, 1, 1, 1, 1, 1]
+      B = delete_duplicates(A)
+      self.assertEqual(B[0], 1)
+      self.assertListEqual(B[1], [1, 0, 0, 0, 0, 0])
+      
+    def test_variant1(self):
+      self.assertEqual(5, variant1(1, [1, 2, 3, 4, 5, 6]))
+      self.assertEqual(5, variant1(4, [1, 2, 3, 4, 5, 6]))
+      self.assertEqual(6, variant1(-1, [1, 1, 1, 2, 2, 2]))
+      self.assertEqual(3, variant1(1, [1, 1, 1, 2, 2, 2]))
+      self.assertEqual(6, variant1(2, [1, 1, 1, 2, 2, 2, 3, 3, 3]))
+      self.assertEqual(5, variant1(2, [2, 1, 2, 2, 3, 2, 3, 1, 3]))
+
 
 
 
