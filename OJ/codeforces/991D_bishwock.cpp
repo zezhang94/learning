972f96b9a3503da9cdf9f11e1224ad0b24756758
yorkzhang <yorkzhang1994@outlook.com>
2020-12-16 16:37:37 +0800

    Add new problem.

--- OJ/codeforces/991D_bishwock.cpp
+++ OJ/codeforces/991D_bishwock.cpp
@@ -36,4 +36,27 @@ int main() {
     cin >> s1 >> s2;
     int n =  s1.size();
     vector<int> dp(n ,0);
+    // how many consequent 2-line 0 from index i (0 <= i < n) 
+    vector<int> zeros(n, -1);
+    for (int i = 1; i != n; ++i) {
+        if (s1[i - 1] == 'X' && s2[i - 1] == '0') {
+            if (s1[i] == '0' && s2[i] == '0') {
+                dp[i] = dp[i - 1] + 1;
+                s2[i - 1] = s1[i] = s2[i] = 'X';
+            } else {
+                dp[i] = dp[i - 1];
+            }
+        } else if (s1[i - 1] == '0' && s2[i - 1] == 'X') {
+            if (s1[i] == '0' && s2[i] == '0') {
+                dp[i] = dp[i - 1] + 1;
+                s1[i - 1] = s1[i] = s2[i] = 'X';
+            } else {
+                dp[i] = dp[i - 1];
+            }
+        } else if (s1[i - 1] == 'X' && s2[i - 1] == 'X') {
+            dp[i] = dp[i - 1];
+        } else {
+            
+        }
+    }
 }
