4740b670bc2004dcf9036717b4c5246c2e006ea4
yorkzhang <yorkzhang1994@outlook.com>
2020-12-17 10:09:30 +0800

    Solve problem.

--- OJ/codeforces/991D_bishwock.cpp
+++ OJ/codeforces/991D_bishwock.cpp
@@ -22,14 +22,17 @@ Examples:
     00000
     2
     
-    000X0
-    00000
+    0123456
+    X0000X0
+    X000000
 */
 
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
+
+enum X_LOCATION { BOTH, UP, DOWN, NONE };
 
 int main() {
     string s1, s2;
@@ -38,6 +41,9 @@ int main() {
     vector<int> dp(n ,0);
     // how many consequent 2-line 0 from index i (0 <= i < n) 
     vector<int> zeros(n, -1);
+    // last: last of consequent 0s
+    enum X_LOCATION last_location = NONE; 
+    int last_index = 0;
     for (int i = 1; i != n; ++i) {
         if (s1[i - 1] == 'X' && s2[i - 1] == '0') {
             if (s1[i] == '0' && s2[i] == '0') {
@@ -56,7 +62,22 @@ int main() {
         } else if (s1[i - 1] == 'X' && s2[i - 1] == 'X') {
             dp[i] = dp[i - 1];
         } else {
-            
+            if (s1[i] == 'X' && s2[i] == 'X') {
+                dp[i] = dp[i - 1];
+            } else if (s1[i] == 'X' && s2[i] == '0') {
+                dp[i] = dp[i - 1] + 1;
+                s2[i] = 'X';
+            } else if (s1[i] == '0' && s2[i] == 'X') {
+                dp[i] = dp[i - 1] + 1;
+                s1[i] = 'X';
+            } else {
+                s1[i - 1] = s2[i - 1] = s1[i] = 'X';
+                dp[i] = dp[i - 1] + 1;
+            }
         }
     }
+
+    cout << dp[n - 1] << '\n';
+
+    return 0;
 }
