1fe35500e1c2bfac90eeed6cba34bfecb8066ec4
yorkzhang <yorkzhang1994@outlook.com>
2021-01-18 20:55:21 +0800

    Try

--- OJ/leetcode/python/639_decode_ways_ii.py
+++ OJ/leetcode/python/639_decode_ways_ii.py
@@ -25,14 +25,25 @@ def numDecodings(s: str):
       else:
         dp[i] += dp[i - 1] * 9 % m 
     else:
+      if 1 <= int(s[i]) <= 9:
+        dp[i] += dp[i - 1] % m
+      if s[i - 1] == '*':
+        if 1 <= int(s[i]) <= 6:
+          dp[i] += (dp[i - 2] + 1) % m
+        else:
+          dp[i] += dp[i - 2] % m
+      elif 10 <= int(s[i - 1 : i + 1]) <= 26:
+        dp[i] += dp[i - 2] % m
 
   return dp[n - 1]
 
 print(numDecodings("*"))
 print(numDecodings("1*"))
+print(numDecodings("*1")) # 11
+print(numDecodings("**1**")) # 18720
 print(numDecodings("11*")) # 9 + (9 + 10) - 1 = 27 
 print(numDecodings("2*"))
 print(numDecodings("10"))
 print(numDecodings("10*"))
-print(numDecodings("**")) # 9 * 9 + 17 = 98
-print(numDecodings("***")) # 9 * 9 * 9 + (17 * 9 * 2) = 1035
+print(numDecodings("**")) # 9 * 9 + 15 = 96
+print(numDecodings("***")) # 9 * 9 * 9 + (15 * 9 * 2) = 
