38bf2dce0efce46c29c68e3185d1b0e3c9c6fa1e
yorkzhang <yorkzhang1994@outlook.com>
2021-01-19 17:17:09 +0800

    New

--- OJ/leetcode/python/639_decode_ways_ii.py
+++ OJ/leetcode/python/639_decode_ways_ii.py
@@ -1,48 +1,51 @@
+ # Reference: https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-639-decode-ways-ii/
 def numDecodings(s: str):
   if s[0] == '0':
-    return False
-  
+    return 0
   m = 1000000007
+
+  def one(c):
+    if c == '0':
+      return 0
+    if c == '*':
+      return 9
+    return 1
+
+  def two(c1, c2):
+    if c1 == '*' and c2 == '*':
+      return 6 + 9
+    elif c1 == '*' and c2 != '*':
+      return 2 if 0 <= int(c2) <= 6 else 1
+    elif c1 != '*' and c2 == '*':
+      if c1 == '1':
+        return 9
+      elif c1 == '2':
+        return 6
+      else:
+        return 0 
+    else:
+      return 1 if 10 <= (10 * int(c1) + int(c2)) <= 26 else 0 
 
   s = "0" + s
   n = len(s)
   # decoding ways of i-size string
-  dp = [0 for _ in range(n)]
-  dp[0] = 1
-  dp[1] = 9 if s[1] == '*' else 1
+  next_pre = 1
+  pre = one(s[1])
   
   for i in range(2, n):
-    if s[i] == '*':
-      if s[i - 1] == '0':
-        dp[i] += dp[i - 1] * 9 % m 
-      elif s[i - 1] == '1':
-        dp[i] += (dp[i - 1] * 9 + dp[i - 2] * 9) % m
-      elif s[i - 1] == '2':
-        dp[i] += (dp[i - 1] * 9 + dp[i - 2] * 6) % m
-      elif s[i - 1] == '*':
-        dp[i] += dp[i - 1] * 9 % m
-        dp[i] += dp[i - 2] * 15 % m
-      else:
-        dp[i] += dp[i - 1] * 9 % m 
-    else:
-      if 1 <= int(s[i]) <= 9:
-        dp[i] += dp[i - 1] % m
-      if s[i - 1] == '*':
-        if 1 <= int(s[i]) <= 6:
-          dp[i] += (dp[i - 2] + 1) % m
-        else:
-          dp[i] += dp[i - 2] % m
-      elif 10 <= int(s[i - 1 : i + 1]) <= 26:
-        dp[i] += dp[i - 2] % m
+    ans = (one(s[i]) * pre + two(s[i - 1], s[i]) * next_pre) % m
+    next_pre = pre
+    pre = ans
 
-  return dp[n - 1]
+  return pre 
 
 print(numDecodings("*"))
 print(numDecodings("1*"))
 print(numDecodings("*1")) # 11
 print(numDecodings("**1**")) # 18720
 print(numDecodings("11*")) # 9 + (9 + 10) - 1 = 27 
-print(numDecodings("2*"))
+print(numDecodings("2*")) # 15
+print(numDecodings("*1*1*0")) # 404
 print(numDecodings("10"))
 print(numDecodings("10*"))
 print(numDecodings("**")) # 9 * 9 + 15 = 96
