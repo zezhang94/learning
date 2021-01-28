5fe06f9bb6776bab6dc51febf78cbcd68ce48cd5
yorkzhang <yorkzhang1994@outlook.com>
2021-01-28 22:16:15 +0800

    New

--- OJ/leetcode/python/37_sudoku_solver.py
+++ OJ/leetcode/python/37_sudoku_solver.py
@@ -1,46 +1,40 @@
 def solveSudoku(board) -> None:
-    def getCandidates(board, x, y):
-        candidates = [True for _ in range(10)]
-        candidates[0] = False
-        for i in range(9):
-            if board[i][y] != ".":
-                candidates[int(board[i][y])] = False
-            if board[x][i] != ".":
-                candidates[int(board[x][i])] = False
-        x //= 3
-        y //= 3
-        for i in range(x, x + 3):
-            for j in range(y, y + 3):
-                if board[i][j] != ".":
-                    candidates[int(board[i][j])] = False
-        return [str(i) for i in range(10) if candidates[i]]
+    def dfs(depth, pairs, row, col, box, board):
+        if depth == len(pairs):
+            return True
 
-    def dfs(i_start, j_start, count, board):
-        if count == 0:
-            for line in board:
-                print(line)
-            return
+        i = pairs[depth][0]
+        j = pairs[depth][1]
+        for v in range(1, 10):
+            if row[i][v] and col[j][v] and box[i // 3 * 3 + j // 3][v]:
+                board[i][j] = str(v)
+                row[i][v] = 0
+                col[j][v] = 0
+                box[i // 3 * 3 + j // 3][v] = 0
+                # Tip: cast result, not recover
+                if dfs(depth + 1, pairs, row, col, box, board):
+                    return True
+                board[i][j] = "."
+                row[i][v] = 1
+                col[j][v] = 1
+                box[i // 3 * 3 + j // 3][v] = 1
+        return False
 
-        if i_start == j_start == 9:
-            for line in board:
-                print(line)
-        for i in range(i_start, 9):
-            for j in range(j_start, 9):
-                if board[i][j] == '.':
-                    print(i, j)
-                    candidates = getCandidates(board, i, j)
-                    for v in candidates:
-                        board[i][j] = v
-                        dfs(i, j + 1, count - 1, board)
-                        board[i][j] = "."
-
-    count = 0
-    for line in board:
-        for s in line:
-            if s == ".":
-                count += 1
-    dfs(0, 0, count, board)
-    print(getCandidates(board, 0, 2))
+    row = [[1 for _ in range(10)] for _ in range(9)]
+    col = [[1 for _ in range(10)] for _ in range(9)]
+    box = [[1 for _ in range(10)] for _ in range(9)]
+    pairs = []
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == ".":
+                pairs.append((i, j))
+            else:
+                index = int(board[i][j])
+                row[i][index] = 0
+                col[j][index] = 0
+                # Tip: flat to one-dimestion 
+                box[i // 3 * 3 + j // 3][index] = 0 
+    dfs(0, pairs, row, col, box, board)
 
 
 board = [ \
@@ -55,4 +49,6 @@ board = [ \
     [".",".",".",".","8",".",".","7","9"]  \
 ]
 solveSudoku(board)
+for line in board:
+    print(line)
     
